c5418e51ae612ed3ea70727c77c0b45724e1285b
Kyle F. Butts <buttskyle96@gmail.com>
2021-01-27 12:15:17 -0700

    Fixed counties with center of pop

--- data-prepare_counties.R
+++ data-prepare_counties.R
@@ -24,16 +24,17 @@ library("doParallel")
 # Load FIPS codes
 data("fips_codes", package= "tigris")
 
+# Run once
+# sf::read_sf("data/2010_county/US_county_2010.shp") %>% rmapshaper::ms_simplify(keep = 0.02) %>% st_transform(st_crs(2163)) %>% sf::write_sf(., "data/2010_county.geojson")
+
 # Load Counties Shapefile at low resolution
-counties <- sf::read_sf("data/2010_county.json") %>% 
-	st_make_valid() %>% 
+counties <- sf::read_sf("data/2010_county.geojson") %>% 
+	sf::st_make_valid() %>% st_buffer(0) %>%
 	select(state_code= STATEFP10, county_code= COUNTYFP10) %>%
 	# Merge with fips_codes to get state and county names
 	left_join(., fips_codes, by= c("state_code", "county_code")) %>%
 	# Remove non-continental states
 	filter(!state_name %in% c("Alaska", "Hawaii", "Puerto Rico", "U.S. Virgin Islands", "American Samoa", "Northern Mariana Islands", "Guam")) %>%
-	# Project to a prettier US project EPSG:2163
-	st_transform(st_crs(2163)) %>%
 	mutate(state_county = paste(state_code, county_code, sep="-")) %>% 
 	arrange(state_code, county_code)
 
@@ -49,10 +50,8 @@ counties_centpop <- sf::read_sf("data/2010_county_centpop/US_county_cenpop_2010.
 ## Create Distance Matrix ------------------------------------------------------
 
 
-counties$centpop <- st_geometry(counties_centpop)
-
 # st_crs(counties) will show you that units are in meters
-dist <- st_distance(st_geometry(counties$centpop))
+dist <- st_distance(st_geometry(counties_centpop))
 
 dist_mi <- dist %>%
 	units::set_units(., "mi") %>% 
